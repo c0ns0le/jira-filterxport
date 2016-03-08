c5e59e4a7cf131ba0f431adc5d78d1f41df25d44
patrickblom <blomsmail@gmail.com>
2016-03-08 01:06:14 +0100

    - start context menu implementation

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,6 +1,35 @@
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+// save current selection to chrome storage
+var onSaveClicked = function (event) {
+
+    var filter = event.selectionText;
+    var id = CryptoJS.SHA256(filter).toString();
+
+    window.console.log("onSaveClicked: check for hash" + id);
+
+    chrome.storage.sync.get("jiraFilters", function (items) {
+        window.console.log(items);
+
+        /*if (item.id === id) {
+            window.console.log("filter already exists name:" + item.name);
+            return;
+        }*/
+
+        window.console.log("saving new filter");
+        items[id] = {
+            "id": id.toString(),
+            "name": "fooBar",
+            "filter": filter
+        };
+
+        window.console.log({"jiraFilters":items});
+        chrome.storage.sync.set({"jiraFilters":items}, function () {
+            window.console.log("all filters saved");
+        });
+    });
+};
+
+chrome.contextMenus.create({
+    "title": "Save Filter",
+    "contexts": ["editable"],
+    "onclick": onSaveClicked
+});
